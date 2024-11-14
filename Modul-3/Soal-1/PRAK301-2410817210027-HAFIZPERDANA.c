32cd75886fe8a853ce26e657b7c19f7ace645d54
Fiezz65 <hfzzprr@gmail.com>
2024-11-14 13:44:05 +0800

    Modul-3

--- Modul-3/Soal-1/PRAK301-2410817210027-HAFIZPERDANA.c
+++ Modul-3/Soal-1/PRAK301-2410817210027-HAFIZPERDANA.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
 int main () {
-    int a, b, c;
+    int a, b;
 
-    scanf("%d %d %d", &a, &b, &c);
+    scanf("%d %d", &a, &b);
 
-    if (c > b && b > a) {
-        printf("%d %d %d",a, b, c);
-    } else if (b > c && c > a) {
-        printf("%d %d %d", a, c, b);
-    } else if (c > a && a > b) {
-        printf("%d %d %d", b, a, c);
-    } else if (a > c && c > b) {
-        printf("%d %d %d", b, c, a);
-    } else if (b > a && a > c) {
-        printf("%d %d %d", c, a, b);
+    if (a > b) {
+        printf("%d %d", b, a);
     } else {
-        printf("%d %d %d", c, b, a);
+        printf("%d %d", a, b);
     }
 
     return 0;
--- Modul-3/Soal-1/PRAK301-2410817210027-HAFIZPERDANA.py
+++ Modul-3/Soal-1/PRAK301-2410817210027-HAFIZPERDANA.py
@@ -1,14 +1,6 @@
-a, b, c = map(int, input("").split())
+a, b= map(int, input("").split())
 
-if c > b and b > a:
-    print(f"{a} {b} {c}")
-elif b > c and c > a:
-    print(f"{a} {c} {b}")
-elif c > a and a > b:
-    print(f"{b} {a} {c}")
-elif a > c and c > b:
-    print(f"{b} {c} {a}")
-elif b > a and a > c:
-    print(f"{c} {a} {b}")
+if a > b:
+    print(f"{b} {a}")
 else:
-    print(f"{c} {b} {a}")
+    print(f"{a} {b}")

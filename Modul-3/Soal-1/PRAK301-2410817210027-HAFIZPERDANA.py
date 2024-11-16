1a4bcfe3957bcdf993aadc7b7cbe64977b649bf9
Fiezz65 <hfzzprr@gmail.com>
2024-11-16 21:54:30 +0800

    Modul-3

--- Modul-3/Soal-1/PRAK301-2410817210027-HAFIZPERDANA.py
+++ Modul-3/Soal-1/PRAK301-2410817210027-HAFIZPERDANA.py
@@ -1,6 +1,14 @@
-a, b= map(int, input("").split())
+a, b, c= map(int, input("").split())
 
-if a > b:
-    print(f"{b} {a}")
+if c > b and b > a:
+    print(f"{a} {b} {c}")
+elif b > c and c > a:
+    print(f"{a} {c} {b}")
+elif c > a and a > b:
+    print(f"{b} {a} {c}")
+elif a > c and c > b:
+    print(f"{b} {c} {a}")
+elif b > a and a > c:
+    print(f"{c} {a} {b}")
 else:
-    print(f"{a} {b}")
+    print(f"{c} {b} {a}")
